5715f5dd089c61d6514b5d9d92f5b29a0e86319f
Oleg Scopich <scopichol@gmail.com>
2012-05-27 17:22:49 +0300

    ticket:3 Ignore logging where DatabaseError

--- django_hello_world/requestlog/middleware.py
+++ django_hello_world/requestlog/middleware.py
@@ -2,11 +2,14 @@ from models import RequestLog
 
 class RequestlogMiddleware(object):
     def process_request(self, request):
-        logEntry = RequestLog(
-            http_host = request.META.get('HTTP_HOST', ''), 
-            path_info = request.META['PATH_INFO'],
-            remote_addr = request.META['REMOTE_ADDR']
-            )
-        logEntry.save()
+        try:
+            logEntry = RequestLog(
+                http_host = request.META.get('HTTP_HOST', ''), 
+                path_info = request.META['PATH_INFO'],
+                remote_addr = request.META['REMOTE_ADDR']
+                )
+            logEntry.save()
+        except DatabaseError:
+            pass
         
         return None
