a2f74a36d95533327b4d89988f5ad39ed31e6d0e
Oleg Scopich <scopichol@gmail.com>
2012-05-27 17:09:23 +0300

    ticket:3 change geting HTTP_HOST for test

--- django_hello_world/requestlog/middleware.py
+++ django_hello_world/requestlog/middleware.py
@@ -3,7 +3,7 @@ from models import RequestLog
 class RequestlogMiddleware(object):
     def process_request(self, request):
         logEntry = RequestLog(
-            http_host=request.META['HTTP_HOST'], 
+            http_host = request.META.get('HTTP_HOST', ''), 
             path_info = request.META['PATH_INFO'],
             remote_addr = request.META['REMOTE_ADDR']
             )
