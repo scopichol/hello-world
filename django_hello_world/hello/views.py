6894441aa261d246c61ae5c0dd477c844075d258
unknown <ScopichOL@.HO.OBU>
2012-05-28 15:17:58 +0300

    ticket:3 Change sort direction

--- django_hello_world/hello/views.py
+++ django_hello_world/hello/views.py
@@ -16,10 +16,10 @@ def home(request):
 
 
 def syncdb(request):
-	execute_manager(settings, ['manage.py','syncdb','--noinput'])
-	return HttpResponseRedirect('/')
+    execute_manager(settings, ['manage.py','syncdb','--noinput'])
+    return HttpResponseRedirect('/')
 
 @render_to('hello/requestlog.html')
 def requestlog(request):
-    logEntries = RequestLog.objects.all().order_by('id')[:10]
+    logEntries = RequestLog.objects.all().order_by('-id')[:10]
     return {'object_list': logEntries}
