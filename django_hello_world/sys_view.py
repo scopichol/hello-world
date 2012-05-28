6ff26b8ae90b248892d0bcdbabf9bb7382eb08b1
unknown <ScopichOL@.HO.OBU>
2012-05-28 15:24:32 +0300

    Remove unused file

--- django_hello_world/sys_view.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.http import HttpResponseRedirect
-from django.core.management import execute_manager
-import django_hello_world.settings
-
-def syncdb(request):
-	execute_manager(django_hello_world.settings, ['manage.py','syncdb','--noinput'])
-	return HttpResponseRedirect('/')
