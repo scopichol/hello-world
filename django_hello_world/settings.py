08c14bb5617b169d5cb80ea4f3ff162b8c378b9a
Oleg Scopich <scopichol@gmail.com>
2012-05-31 15:37:27 +0300

    ticket:1 Calc an using PROJECT_ROOT

--- django_hello_world/settings.py
+++ django_hello_world/settings.py
@@ -1,5 +1,8 @@
 # Django settings for django_hello_world project.
 from django.conf import global_settings
+import os.path
+
+PROJECT_ROOT, filename = os.path.split(__file__)
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -46,7 +49,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = 'django_hello_world/uploads'
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'uploads')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -57,7 +60,7 @@ MEDIA_URL = '/uploads/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = 'django_hello_world/static'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
