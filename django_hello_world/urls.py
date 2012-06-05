c54ecd010919bb6c636eaaf18ff9510229a96ebd
Oleg Scopich <scopichol@gmail.com>
2012-06-05 10:26:35 +0300

    Fix serve MEDIA_URL for developer server

--- django_hello_world/urls.py
+++ django_hello_world/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -23,3 +25,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
 urlpatterns += staticfiles_urlpatterns()
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
